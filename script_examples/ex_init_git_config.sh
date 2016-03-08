43f624becc8dbeca48a11ca034ed29da9ca95d0b
ktaiga <ktaiga@gmail.com>
2016-03-08 13:52:47 -0800

    adding git init script

--- script_examples/ex_init_git_config.sh
+++ script_examples/ex_init_git_config.sh
@@ -21,15 +21,21 @@
 exit 0
 #you will not execute below commands... after exit...
 #check if git is available:
+
 git --version &> /dev/null
 is_git_available=$?
-if [ ! $is_git_available -eq 0 ]; then
+
+if [ ! $is_git_available -eq 0 ]; then #if git is not installed...
+
 	#install git here
+	echo "git is not installed yet. now installing..."
+	sudo yum install git-all
+	wait
+	git config --global user.name "$(whoami)"
+	git config --global user.email "$(whoami)@$(hostname)" 
+	git config --global core.editor vi
+	git config --global merge.tool vimdiff
+	git config --list
+
 fi
 
-git config --global user.name "ktaiga"
-git config --global user.email ktaiga@gmail.com
-git config --global core.editor vi
-git config --global merge.tool vimdiff
-
-git config --list#
