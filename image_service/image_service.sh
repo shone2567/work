b46c7324765fb234f7e3089fc6407b77160876de
ycwang730 <yinchieh89@gmail.com>
2016-03-18 19:02:56 -0700

    add image service

---
+++ image_service/image_service.sh
@@ -0,0 +1,22 @@
+mysql -u root &> /dev/null << CREATEDB
+CREATE DATABASE glance;
+GRANT ALL PRIVILEGES ON glance.* TO 'glance'@'localhost' IDENTIFIED BY 'GLANCE_DBPASS';
+GRANT ALL PRIVILEGES ON glance.* TO 'glance'@'%' IDENTIFIED BY 'GLANCE_DBPASS';
+exit
+CREATEDB
+
+source admin-openrc.sh
+
+#3
+openstack user create --domain default --password-prompt glance
+openstack role add --project service --user glance admin
+openstack service create --name glance --description "OpenStack Image service" image
+
+#4
+openstack endpoint create --region RegionOne image public http://controller:9292openstack endpoint create --region RegionOne image internal http://controller:9292
+openstack endpoint create --region RegionOne image admin http://controller:9292
+
+su -s /bin/sh -c "glance-manage db_sync" glance
+
+systemctl enable openstack-glance-api.service openstack-glance-registry.service
+systemctl start openstack-glance-api.service openstack-glance-registry.service
