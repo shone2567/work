cf4f2c5f7af159599bf279cc29e5b7834d5f6d0d
shone2567 <shonemike1212@gmail.com>
2016-03-18 22:53:44 -0400

    update

--- identity_service/sub_keystone_verify.sh
+++ identity_service/sub_keystone_verify.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/expect
 
 spawn openstack --os-auth-url http://controller:35357/v3 \
-  --os-project-domain-id default --os-user-domain-id default \
+  --os-project-domain-name default --os-user-domain-name default \
   --os-project-name admin --os-username admin --os-auth-type password \
   token issue
 expect "*Password*:" { send "Super123\r" }
