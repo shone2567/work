fd35bf5620250c610bd874e8e71f5c7a67fbd9f4
root <root@localhost.localdomain>
2016-03-18 23:13:45 -0400

    update

--- compute_service/sub_computeuser_create.sh
+++ compute_service/sub_computeuser_create.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect
-spawn openstack user create --domain default --password-prompt compute
+spawn openstack user create --domain default --password-prompt nova
 expect "User Password*" { send "Super123\r" }
 expect "*Password*:" { send "Super123\r" }
 interact
