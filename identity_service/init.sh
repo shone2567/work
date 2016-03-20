3b48ef7d213b34cdc4f90c6b4706303cfe0badcd
shone2567 <shonemike1212@gmail.com>
2016-03-19 20:15:42 -0400

    bugs fixing

--- identity_service/init.sh
+++ identity_service/init.sh
@@ -3,7 +3,9 @@
 set -x
 
 #Create a database and an administration token
-mysql - u root -pSuper123 << EOF
+
+
+mysql -u root -pSuper123 << EOF
 CREATE DATABASE keystone;
 GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' \
   IDENTIFIED BY 'Super123';
