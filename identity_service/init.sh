e3db2e850fa66afe9b58c24c4948630582e104f5
shonemike1212@gmail.com <shonemike1212@gmail.com>
2016-03-21 19:05:36 -0400

    rolling back

--- identity_service/init.sh
+++ identity_service/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+set -x
 #Create a database and an administration token
 mysql - u root -pSuper123 << EOF
 CREATE DATABASE keystone;
@@ -12,9 +12,19 @@ EOF
 
 #Generate admin token
 
-$admintoken=$(openssl rand -hex 10)
+admintoken=$(openssl rand -hex 10)
 
 
+#Install the packages
 
+#yum install -y openstack-keystone httpd mod_wsgi memcached python-memcached
+#systemctl enable memcached.service
+#systemctl enable memcached.service
+
+#edit/create conf files
+
+#./edit_keystone.conf
+#./edit_httpd.conf
+#./create_wsgi-keystone.conf
 
 
