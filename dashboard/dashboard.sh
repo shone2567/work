b6654eed2189885c824f0f2abb5d0499a97f4f5a
root <root@localhost.localdomain>
2016-03-21 22:31:30 -0700

    edit dashboard.sh

--- dashboard/dashboard.sh
+++ dashboard/dashboard.sh
@@ -1,8 +1,9 @@
-su
+#su
 
 yum install openstack-dashboard
 
 #Edit the /etc/openstack-dashboard/local_settings file
+cp -f local_settings /etc/openstack-dashboard/
 
 systemctl enable httpd.service memcached.service
 systemctl restart httpd.service memcached.service
