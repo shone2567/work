cbb5e86717008eb1b61e06361c43df6eb40d73b1
allenw <yinchieh89@gmail.com>
2016-03-23 23:03:27 -0400

    Finish Compute Service for Controller

---
+++ compute_service/compute_service_for_controller/compute_service_controller_node.sh
@@ -0,0 +1,35 @@
+#su
+
+mysql -u root &> /dev/null << CREATEDB
+
+CREATE DATABASE nova;
+GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'localhost' IDENTIFIED BY 'NOVA_DBPASS';
+GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'%' IDENTIFIED BY 'NOVA_DBPASS';
+
+CREATEDB 
+
+source admin-openrc.sh
+
+openstack user create --domain default --password-prompt nova
+openstack role add --project service --user nova admin
+
+openstack service create --name nova --description "OpenStack Compute" compute
+
+openstack endpoint create --region RegionOne compute public http://controller:8774/v2/%\(tenant_id\)s
+
+openstack endpoint create --region RegionOne compute internal http://controller:8774/v2/%\(tenant_id\)s
+
+openstack endpoint create --region RegionOne compute admin http://controller:8774/v2/%\(tenant_id\)s
+
+yum install -y openstack-nova-api openstack-nova-cert openstack-nova-conductor openstack-nova-console openstack-nova-novncproxy openstack-nova-scheduler python-novaclient
+
+#Edit the /etc/nova/nova.conf file
+
+cp -f nova.conf /etc/nova/nova.conf
+
+su -s /bin/sh -c "nova-manage db sync" nova
+
+systemctl enable openstack-nova-api.service openstack-nova-cert.service openstack-nova-consoleauth.service openstack-nova-scheduler.service openstack-nova-conductor.service openstack-nova-novncproxy.service
+
+systemctl start openstack-nova-api.service openstack-nova-cert.service openstack-nova-consoleauth.service openstack-nova-scheduler.service openstack-nova-conductor.service openstack-nova-novncproxy.service
+
