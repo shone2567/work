f86eecfa8a0fcf254956d3c5c5cd10b6aa420a73
allenw <yinchieh89@gmail.com>
2016-03-31 18:17:59 -0400

    edit dashboard.sh

--- dashboard/dashboard.sh
+++ dashboard/dashboard.sh
@@ -7,3 +7,6 @@ cp -f local_settings /etc/openstack-dashboard/
 
 systemctl enable httpd.service memcached.service
 systemctl restart httpd.service memcached.service
+
+firewall-cmd --zone=public --add-service=http --permanent
+systemctl restart firewalld.service
