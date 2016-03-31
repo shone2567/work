3201bcc9a6e49a6e71b486bfa953d4148a31dc4e
shone2567 <shonemike1212@gmail.com>
2016-03-30 20:50:30 -0400

    bugs fixing

--- networking_service/controller_init.sh
+++ networking_service/controller_init.sh
@@ -191,12 +191,22 @@ su -s /bin/sh -c "neutron-db-manage --config-file /etc/neutron/neutron.conf \
   --config-file /etc/neutron/plugins/ml2/ml2_conf.ini upgrade head" neutron
 
 systemctl restart openstack-nova-api.service
+
 systemctl enable neutron-server.service \
   neutron-linuxbridge-agent.service neutron-dhcp-agent.service \
   neutron-metadata-agent.service
+
 systemctl start neutron-server.service \
   neutron-linuxbridge-agent.service neutron-dhcp-agent.service \
   neutron-metadata-agent.service
 
 sudo iptables -A IN_public_allow -p tcp -m tcp --dport 5672 -m conntrack --ctstate NEW -j ACCEPT
+
+#verify
+
+source admin-openrc.sh
+
+neutron ext-list
+
+
 
--- networking_service/sub_neutronuser_create.sh
+++ networking_service/sub_neutronuser_create.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/expect
+
 spawn openstack user create --domain default --password-prompt neutron
+
 expect "User Password*" { send "Super123\r" }
-expect "*Password*:" { send "Super123\r" }
+expect "Repeat User Password:" { send "Super123\r" }
 interact
 
