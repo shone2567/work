ae0fd7ea2a5ab57a8c0ea97c319b1241b5bd4fee
supermicro <supermicro@localhost.localdomain>
2016-04-04 16:59:56 -0700

    edit image_service.sh

--- image_service/image_service.sh
+++ image_service/image_service.sh
@@ -17,6 +17,7 @@ openstack role add --project service --user glance admin
 openstack service create --name glance --description "OpenStack Image service" image
 openstack endpoint create --region RegionOne image public http://controller:9292
 openstack endpoint create --region RegionOne image internal http://controller:9292
+openstack endpoint create --region RegionOne image admin http://controller:9292
 
 yum install -y openstack-glance python-glance python-glanceclient
 
