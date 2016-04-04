ddffed992edf1a64946a78e01f3196475056204a
supermicro <supermicro@localhost.localdomain>
2016-04-04 16:50:59 -0700

    edit compute_service_controller_node

--- compute_service/compute_service_for_controller/compute_service_controller_node.sh
+++ compute_service/compute_service_for_controller/compute_service_controller_node.sh
@@ -1,12 +1,12 @@
 #su
 
-mysql -u root-pSuper123 &> /dev/null << CREATEDB
+mysql -u root-pSuper123 &> /dev/null << CREATEDB_COMPUTE_CONTROLLER
 
 CREATE DATABASE nova;
 GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'localhost' IDENTIFIED BY 'Super123';
 GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'%' IDENTIFIED BY 'Super123';
 
-CREATEDB 
+CREATEDB_COMPUTE_CONTROLLER
 
 source admin-openrc.sh
 
