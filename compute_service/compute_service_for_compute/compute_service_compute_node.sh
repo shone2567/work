38c7939b78ccaa4ec46c3bbf286f713fd8e36dc0
ycwang730 <yinchieh89@gmail.com>
2016-04-05 01:56:13 -0400

    edit

--- compute_service/compute_service_for_compute/compute_service_compute_node.sh
+++ compute_service/compute_service_for_compute/compute_service_compute_node.sh
@@ -2,13 +2,14 @@
 
 yum -y install openstack-nova-compute sysfsutils
 
-acceleration = `egrep -c '(vmx|svm)' /proc/cpuinfo`
+acceleration=`egrep -c '(vmx|svm)' /proc/cpuinfo`
+echo $acceleration
 
-if [acceleration == 0]
+if [ acceleration -eq 0 ];
 then
-	cp -f nova_not_support_hw_acceleration/nova.conf /etc/nova
+	cp -f nova_not_support_hw_acceleration/nova.conf /etc/nova;
 else
-	cp -f nova_support_hw_acceleration/nova.conf /etc/nova
+	cp -f nova_support_hw_acceleration/nova.conf /etc/nova;
 fi
 
 #Edit the [libvirt] section in the /etc/nova/nova.conf file
