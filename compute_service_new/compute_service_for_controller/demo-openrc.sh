c022ccdc54f9e6c8cba0817b52d13ea0f97c7a0d
allenw <yinchieh89@gmail.com>
2016-04-13 11:13:21 -0700

    edit

--- compute_service_new/compute_service_for_controller/demo-openrc.sh
+++
@@ -1,9 +0,0 @@
-export OS_PROJECT_DOMAIN_ID=default
-export OS_USER_DOMAIN_ID=default
-export OS_PROJECT_NAME=admin
-export OS_TENANT_NAME=admin
-export OS_USERNAME=admin
-export OS_PASSWORD=Super123
-export OS_AUTH_URL=http://controller:35357/v3
-export OS_IDENTITY_API_VERSION=3
-export OS_IMAGE_API_VERSION=2
--- image_service/prompt_image_service.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/expect
-
-
-set timeout 40
-spawn openstack user create --domain default --password-prompt glance
-expect "User Password:" { send "Super123\r" }
-expect "Repeat User Password:" { send "Super123\r" }
-interact
-
