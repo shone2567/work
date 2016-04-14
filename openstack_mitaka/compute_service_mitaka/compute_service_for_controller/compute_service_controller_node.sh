7bdfc98346cefb875a80bfaba90b4b0f70bfca96
allenw <yinchieh89@gmail.com>
2016-04-14 11:03:22 -0700

    edit compute service

--- openstack_mitaka/compute_service_mitaka/compute_service_for_controller/compute_service_controller_node.sh
+++ openstack_mitaka/compute_service_mitaka/compute_service_for_controller/compute_service_controller_node.sh
@@ -10,15 +10,15 @@ GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'%' IDENTIFIED BY 'Super123';
 EXIT
 CREATEDB_COMPUTE_CONTROLLER
 
-. admin-openrc.sh
+. admin-openrc
 
 openstack user create --domain default --password Super123 nova
 openstack role add --project service --user nova admin
 openstack service create --name nova --description "OpenStack Compute" compute
 
-openstack endpoint create --region RegionOne compute public http://controller:8774/v2/%\(tenant_id\)s
-openstack endpoint create --region RegionOne compute internal http://controller:8774/v2/%\(tenant_id\)s
-openstack endpoint create --region RegionOne compute admin http://controller:8774/v2/%\(tenant_id\)s
+openstack endpoint create --region RegionOne compute public http://controller:8774/v2.1/%\(tenant_id\)s
+openstack endpoint create --region RegionOne compute internal http://controller:8774/v2.1/%\(tenant_id\)s
+openstack endpoint create --region RegionOne compute admin http://controller:8774/v2.1/%\(tenant_id\)s
 
 yum install -y openstack-nova-api openstack-nova-cert openstack-nova-conductor openstack-nova-console openstack-nova-novncproxy openstack-nova-scheduler
 
@@ -33,4 +33,4 @@ systemctl enable openstack-nova-api.service openstack-nova-cert.service openstac
 
 systemctl start openstack-nova-api.service openstack-nova-cert.service openstack-nova-consoleauth.service openstack-nova-scheduler.service openstack-nova-conductor.service openstack-nova-novncproxy.service
 
-#iptables -A IN_public_allow -p tcp -m tcp --dport 5672 -m conntrack --ctstate NEW -j ACCEPT
+iptables -A IN_public_allow -p tcp -m tcp --dport 5672 -m conntrack --ctstate NEW -j ACCEPT
