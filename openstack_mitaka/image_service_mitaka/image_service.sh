cdfcc98fa2c87a9f4110629ff7d4cf818d4a1832
allenw <yinchieh89@gmail.com>
2016-04-14 11:08:07 -0700

    edit image_service

--- openstack_mitaka/image_service_mitaka/image_service.sh
+++ openstack_mitaka/image_service_mitaka/image_service.sh
@@ -31,3 +31,12 @@ su -s /bin/sh -c "glance-manage db_sync" glance
 
 systemctl enable openstack-glance-api.service openstack-glance-registry.service
 systemctl start openstack-glance-api.service openstack-glance-registry.service
+
+. admin-openrc
+
+yum install -y wget
+wget http://download.cirros-cloud.net/0.3.4/cirros-0.3.4-x86_64-disk.img
+
+openstack image create "cirros" --file cirros-0.3.4-x86_64-disk.img --disk-format qcow2 --container-format bare --public
+
+openstack image list
