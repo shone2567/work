334f1fc9315022f480ff877a688ebae2b4e4e52b
shone2567 <shonemike1212@gmail.com>
2016-04-14 21:40:34 -0400

    update

--- swift_service/sub_storagenodes_configure.sh
+++ swift_service/sub_storagenodes_configure.sh
@@ -3,6 +3,7 @@
 IFS=$'\n' read -d '' -r -a nodes < object_storage_nodes
 
 #cd ~/.ssh
+chmod 777 ~/.ssh
 ssh-keygen -f id_rsa -t rsa -N ''
 cp id_rsa id_rsa.pub "/root/.ssh/"
 rm -f id_rsa id_rsa.pub
@@ -10,6 +11,6 @@ for node_ip in ${nodes[*]}
 do
 	./ssh_key_auth.sh $node_ip
 	scp ssh_key_auth.sh storage_init.sh root@$node_ip:~
-	ssh root@$node_ip '~/storage_init.sh' >> "$node_ip""_output" &
+	ssh root@$node_ip '~/storage_init.sh' &>> "$node_ip""_output" &
 done
 
