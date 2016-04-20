0590fa13f79938f996707d71eb45b3f75a37c227
allenw <yinchieh89@gmail.com>
2016-04-20 15:29:26 -0700

    add_auto_all_service

---
+++ openstack_mitaka/deploy.sh
@@ -0,0 +1,12 @@
+#environment
+
+#identity service
+
+#image service
+. /openstack_mitaka/image_service_mitaka/image_service_deploy.sh
+
+#compute service
+#controller
+
+. /openstack_mitaka/compute_service_mitaka/
+scp 
---
+++ openstack_mitaka/image_service_mitaka/image_service_deploy.sh
@@ -0,0 +1,95 @@
+#su
+
+mysql -u root -pSuper123 &> /dev/null << CREATEDB
+CREATE DATABASE glance;
+GRANT ALL PRIVILEGES ON glance.* TO 'glance'@'localhost' IDENTIFIED BY 'Super123';
+GRANT ALL PRIVILEGES ON glance.* TO 'glance'@'%' IDENTIFIED BY 'Super123';
+EXIT
+CREATEDB
+
+#change directory
+. admin-openrc
+#. /openstack_mitaka/image_service_mitaka/admin-openrc
+
+#3
+openstack user create --domain default --password Super123 glance
+openstack role add --project service --user glance admin
+
+#4
+openstack service create --name glance --description "OpenStack Image service" image
+openstack endpoint create --region RegionOne image public http://controller:9292
+openstack endpoint create --region RegionOne image internal http://controller:9292
+openstack endpoint create --region RegionOne image admin http://controller:9292
+
+yum install -y openstack-glance
+
+#Edit the /etc/glance/glance-api.conf file
+#cp -f glance-api.conf /etc/glance/
+
+openstack-config --set /etc/glance/glance-api.conf database connection mysql+pymysql://glance:Super123@controller/glance
+
+openstack-config --set /etc/glance/glance-api.conf keystone_authtoken auth_uri http://controller:5000
+
+openstack-config --set /etc/glance/glance-api.conf keystone_authtoken auth_url http://controller:35357
+
+openstack-config --set /etc/glance/glance-api.conf keystone_authtoken memcached_servers controller:11211
+
+openstack-config --set /etc/glance/glance-api.conf keystone_authtoken auth_type password
+
+openstack-config --set /etc/glance/glance-api.conf keystone_authtoken project_domain_name default
+
+openstack-config --set /etc/glance/glance-api.conf keystone_authtoken user_domain_name default
+
+openstack-config --set /etc/glance/glance-api.conf keystone_authtoken project_name service
+
+openstack-config --set /etc/glance/glance-api.conf keystone_authtoken username glance
+
+openstack-config --set /etc/glance/glance-api.conf keystone_authtoken password Super123
+
+openstack-config --set /etc/glance/glance-api.conf paste_deploy flavor keystone
+
+openstack-config --set /etc/glance/glance-api.conf glance_store stores file,http
+
+openstack-config --set /etc/glance/glance-api.conf glance_store default_store file
+
+openstack-config --set /etc/glance/glance-api.conf glance_store filesystem_store_datadir /var/lib/glance/images/
+
+openstack-config --set /etc/glance/glance-registry.conf database connection mysql+pymysql://glance:Super123@controller/glance
+
+openstack-config --set /etc/glance/glance-registry.conf keystone_authtoken auth_uri http://controller:5000
+
+openstack-config --set /etc/glance/glance-registry.conf keystone_authtoken auth_url http://controller:35357
+
+openstack-config --set /etc/glance/glance-registry.conf keystone_authtoken memcached_servers controller:11211
+
+openstack-config --set /etc/glance/glance-registry.conf keystone_authtoken auth_type password
+
+openstack-config --set /etc/glance/glance-registry.conf keystone_authtoken project_domain_name default
+
+openstack-config --set /etc/glance/glance-registry.conf keystone_authtoken user_domain_name default
+
+openstack-config --set /etc/glance/glance-registry.conf keystone_authtoken project_name service
+
+openstack-config --set /etc/glance/glance-registry.conf keystone_authtoken username glance
+
+openstack-config --set /etc/glance/glance-registry.conf keystone_authtoken password Super123
+
+openstack-config --set /etc/glance/glance-registry.conf paste_deploy flavor keystone
+
+#Edit the /etc/glance/glance-registry.conf
+#cp -f glance-registry.conf /etc/glance/
+
+su -s /bin/sh -c "glance-manage db_sync" glance
+
+systemctl enable openstack-glance-api.service openstack-glance-registry.service
+systemctl start openstack-glance-api.service openstack-glance-registry.service
+
+#. /openstack_mitaka/image_service_mitaka/admin-openrc
+. admin-openrc
+
+yum install -y wget
+wget http://download.cirros-cloud.net/0.3.4/cirros-0.3.4-x86_64-disk.img
+
+openstack image create "cirros" --file cirros-0.3.4-x86_64-disk.img --disk-format qcow2 --container-format bare --public
+
+openstack image list
