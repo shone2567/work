7581ff3a65073e8fc04c3de07490d81f7b984d50
allenw <yinchieh89@gmail.com>
2016-04-21 15:37:59 -0700

    edit

---
+++ openstack_mitaka/block_storage_mitaka/cinder_controller/block_storage_service_deploy.sh
@@ -0,0 +1,58 @@
+mysql -u root -pSuper123 &> /dev/null << CREATECINDER 
+CREATE DATABASE cinder;
+GRANT ALL PRIVILEGES ON cinder.* TO 'cinder'@'localhost' IDENTIFIED BY 'Super123';
+GRANT ALL PRIVILEGES ON cinder.* TO 'cinder'@'%' IDENTIFIED BY 'Super123';
+EXIT
+CREATECINDER
+
+#. admin-openrc
+. /block_storage_mitaka/cinder_controller/admin-openrc
+
+
+openstack user create --domain default --password Super123 cinder
+openstack role add --project service --user cinder admin
+
+openstack service create --name cinder --description "OpenStack Block Storage" volume
+
+openstack service create --name cinderv2  --description "OpenStack Block Storage" volumev2
+
+openstack endpoint create --region RegionOne volume public http://controller:8776/v1/%\(tenant_id\)s
+openstack endpoint create --region RegionOne volume internal http://controller:8776/v1/%\(tenant_id\)s
+openstack endpoint create --region RegionOne volume admin http://controller:8776/v1/%\(tenant_id\)s
+
+openstack endpoint create --region RegionOne volumev2 public http://controller:8776/v2/%\(tenant_id\)s
+openstack endpoint create --region RegionOne volumev2 internal http://controller:8776/v2/%\(tenant_id\)s
+openstack endpoint create --region RegionOne volumev2 admin http://controller:8776/v2/%\(tenant_id\)s
+
+yum install -y openstack-cinder
+
+#cp -f cinder.conf /etc/cinder/
+openstack-config --set /etc/nova/nova.conf database connection mysql+pymysql://cinder:Super123@controller/cinder
+
+openstack-config --set /etc/nova/nova.conf DEFAULT rpc_backend rabbit
+openstack-config --set /etc/nova/nova.conf oslo_messaging_rabbit rabbit_host controller
+openstack-config --set /etc/nova/nova.conf oslo_messaging_rabbit rabbit_userid openstack
+openstack-config --set /etc/nova/nova.conf oslo_messaging_rabbit rabbit_password Super123
+openstack-config --set /etc/nova/nova.conf DEFAULT auth_strategy keystone
+openstack-config --set /etc/nova/nova.conf keystone_authtoken auth_uri http://controller:5000
+openstack-config --set /etc/nova/nova.conf keystone_authtoken auth_url http://controller:35357
+openstack-config --set /etc/nova/nova.conf keystone_authtoken memcached_servers controller:11211
+openstack-config --set /etc/nova/nova.conf keystone_authtoken auth_type passwordopenstack-config --set /etc/nova/nova.conf keystone_authtoken project_domain_name default
+openstack-config --set /etc/nova/nova.conf keystone_authtoken user_domain_name default
+openstack-config --set /etc/nova/nova.conf keystone_authtoken project_name service
+openstack-config --set /etc/nova/nova.conf keystone_authtoken username cinder
+openstack-config --set /etc/nova/nova.conf keystone_authtoken password Super123
+
+openstack-config --set /etc/nova/nova.conf DEFAULT my_ip 10.0.0.11
+openstack-config --set /etc/nova/nova.conf oslo_concurrency lock_path /var/lib/cinder/tmp
+
+su -s /bin/sh -c "cinder-manage db sync" cinder
+
+openstack-config --set /etc/nova/nova.conf cinder os_region_name RegionOne
+
+systemctl restart openstack-nova-api.service
+systemctl enable openstack-cinder-api.service openstack-cinder-scheduler.servicesystemctl start openstack-cinder-api.service openstack-cinder-scheduler.service
+
+firewall-cmd --zone=public --add-port=3306/tcp --permanent
+firewall-cmd --zone=public --add-port=123/udp --permanent
+systemctl restart firewalld
