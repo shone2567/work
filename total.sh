9209f02e0934a5d240fe0deae3b92c08118a70b0
shone2567 <shonemike1212@gmail.com>
2016-04-22 14:06:57 -0700

    update

---
+++ total.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+#/root/work/identity_service/init.sh
+#/root/work/glance_service/init.sh
+/root/work/compute_service/init.sh
+/root/work/networking_service/init.sh
+/root/work/horizon_service/init.sh
+/root/work/swift_service/init.sh
+
+
+
