0f6831a1a1fa57c5904baaab8e17447044346413
allenw <yinchieh89@gmail.com>
2016-05-03 16:25:26 -0700

    edit

--- openstack_mitaka/block_storage_mitaka/init.sh
+++ openstack_mitaka/block_storage_mitaka/init.sh
@@ -1,11 +1,11 @@
 #execute
-. cinder_controller/block_storage_service.sh
+. cinder_controller/block_storage_service.sh &> /dev/null
 
 ./ssh-keygen
 scp -r cinder_storage root@block1
 
 #change to block
 ssh root@block1
-. cinder_storage/block_storage_service_deploy.sh
+. cinder_storage/block_storage_service_deploy.sh &> /dev/null
 
 
