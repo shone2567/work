89f096983ee6a95bb348d9cdaba19e5cedc888c7
allenw <yinchieh89@gmail.com>
2016-05-03 11:59:45 -0700

    add

--- openstack_mitaka/share_file_system/init.sh
+++ openstack_mitaka/share_file_system/init.sh
@@ -1,1 +1,8 @@
-asd
+. share_file_system_for_controller/share_file_system_for_controller.sh
+
+
+./ssh-keygen
+scp -r share_file_system_for_cinder root@block1
+
+ssh root@block1
+./share_file_system_for_cinder.sh
