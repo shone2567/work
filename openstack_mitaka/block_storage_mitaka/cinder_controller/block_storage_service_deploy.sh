edf4357ab44dbe160ef0d0233915dd0a37bdee0e
allenw <yinchieh89@gmail.com>
2016-05-03 18:48:13 -0700

    edit

--- openstack_mitaka/block_storage_mitaka/cinder_controller/block_storage_service_deploy.sh
+++ openstack_mitaka/block_storage_mitaka/cinder_controller/block_storage_service_deploy.sh
@@ -13,7 +13,7 @@ openstack user create --domain default --password Super123 cinder
 openstack role add --project service --user cinder admin
 
 openstack service create --name cinder --description "OpenStack Block Storage" volume
-openstack service create --name cinderv2  --description "OpenStack Block Storage" volumev2
+openstack service create --name cinderv2 --description "OpenStack Block Storage" volumev2
 
 openstack endpoint create --region RegionOne volume public http://controller:8776/v1/%\(tenant_id\)s
 openstack endpoint create --region RegionOne volume internal http://controller:8776/v1/%\(tenant_id\)s
