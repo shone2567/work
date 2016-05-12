540e3f0eeb223bf6a9e75b2681d35f908d6f86cd
allenw <yinchieh89@gmail.com>
2016-05-12 10:36:48 -0700

    chnage

--- openstack_mitaka/compute_service_mitaka/init.sh
+++ openstack_mitaka/compute_service_mitaka/init.sh
@@ -1,2 +1,4 @@
 . compute_service_for_controller/compute_service_controller_node_deploy.sh
 
+scp -rf computer_service_for_compute root@compute1:/root
+ssh -t root@compute1 ". compute_service_for_compute/compute_service_compute_node_deploy.sh"
